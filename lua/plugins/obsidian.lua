c37f5ff8abde5790e76e2ae3acbc10128047ee82
Ryan Houston <ryanhouston83@gmail.com>
2024-12-12 23:26:27 -0500

    Configure obsidian.nvim for my defaults
    Currently unsure how to handle the fact that my vaults are not the same
    across machines right now. I might need some gitignored plugin
    local-overrides.lua or something.

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -21,10 +21,47 @@ return {
         name = "compass",
         path = "~/Documents/Wiki/Compass",
       },
+      {
+        name = "personal",
+        path = "~/Dropbox/Wiki/personal",
+      },
     },
-    {
-      name = "personal",
-      path = "~/Dropbox/Wiki/personal",
+
+    notes_subdir = "Log",
+
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      local suffix = ""
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.date("%Y%m%d-%H%M%S", os.time())) .. "-" .. suffix
+    end,
+
+    preferred_link_style = "markdown",
+
+    templates = {
+      folder = "_templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+      -- A map for custom variables, the key should be the variable and the value a function
+      substitutions = {},
     },
+
+    ---@param url string
+    follow_url_func = function(url)
+      -- Open the URL in the default web browser.
+      vim.ui.open(url) -- need Neovim 0.10.0+
+    end,
   },
 }
