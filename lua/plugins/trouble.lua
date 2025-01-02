a9db68bcff5e419442c9d572663349f63b13d174
Ryan Houston <ryanhouston83@gmail.com>
2025-01-02 17:23:17 -0500

    [plugin] update trouble.nvim window width

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,14 @@
+return {
+  " folke/trouble.nvim",
+  opts = {
+    modes = {
+      symbols = {
+        win = {
+          -- Values < 1 are a percentage of the screen.
+          -- Values > 1 are static column width.
+          size = 40,
+        },
+      },
+    },
+  },
+}
