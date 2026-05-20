7a2514a720d1b6c307ef9f7977e2c0bb0fd41804
Ryan Houston <ryanhouston83@gmail.com>
2026-05-19 22:18:34 -0400

    Replace folke/sidekick with opencode-nvim
    sidekick did not feel as intuitive so I'm trying opencode-nvim

--- lua/plugins/ai-agents.lua
+++ lua/plugins/ai-agents.lua
@@ -1,6 +1,54 @@
 return {
-  "folke/sidekick.nvim",
-  opts = {
-    nes = { enabled = false },
+  "nickjvandyke/opencode.nvim",
+  version = "*",
+  dependencies = {
+    {
+      "folke/snacks.nvim",
+      optional = true,
+      opts = {
+        input = {},
+        picker = {
+          actions = {
+            opencode_send = function(...)
+              return require("opencode").snacks_picker_send(...)
+            end,
+          },
+          win = {
+            input = {
+              keys = {
+                ["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
+              },
+            },
+          },
+        },
+      },
+    },
   },
+  config = function()
+    vim.o.autoread = true
+
+    vim.keymap.set({ "n", "x" }, "<leader>aa", function()
+      require("opencode").ask("@this: ", { submit = true })
+    end, { desc = "Ask opencode" })
+    vim.keymap.set({ "n", "x" }, "<leader>as", function()
+      require("opencode").select()
+    end, { desc = "Select opencode" })
+    vim.keymap.set({ "n", "t" }, "<leader>at", function()
+      require("opencode").toggle()
+    end, { desc = "Toggle opencode" })
+
+    vim.keymap.set({ "n", "x" }, "go", function()
+      return require("opencode").operator("@this ")
+    end, { desc = "Add range to opencode", expr = true })
+    vim.keymap.set("n", "goo", function()
+      return require("opencode").operator("@this ") .. "_"
+    end, { desc = "Add line to opencode", expr = true })
+
+    vim.keymap.set("n", "<leader>au", function()
+      require("opencode").command("session.half.page.up")
+    end, { desc = "Scroll opencode up" })
+    vim.keymap.set("n", "<leader>ad", function()
+      require("opencode").command("session.half.page.down")
+    end, { desc = "Scroll opencode down" })
+  end,
 }
