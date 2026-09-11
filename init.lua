308c5f8bc4d04825a0c70a969dbaca9bb9697cbe
Ryan Houston <ryanhouston83@gmail.com>
2026-09-11 14:09:17 -0400

    Add DiffOrig command to diff local edits

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_create_user_command("DiffOrig", function()
+  vim.cmd("vert new | set bt=nofile | r ++edit # | 0d_ | diffthis | wincmd p | diffthis")
+end, {})
